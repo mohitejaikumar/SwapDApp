2caf2e6ce60a2f66c66a5222abce096c9765bd5f
root <root@LAPTOP-NN98AKP2.>
2025-02-03 19:39:02 +0000

    Implement smart contract to swap tokens between 2 accounts

---
+++ programs/swap/src/lib.rs
@@ -0,0 +1,37 @@
+pub mod constants;
+pub mod error;
+pub mod instructions;
+pub mod state;
+
+use anchor_lang::prelude::*;
+
+pub use constants::*;
+pub use instructions::*;
+pub use state::*;
+
+declare_id!("9qv82dgWeM3HMGjL78SJh8vCgdzjT5o17CYxU68ogeEy");
+
+#[program]
+pub mod swap {
+    use super::*;
+
+    pub fn make_offer(
+        ctx: Context<MakeOffer>,
+        id: u64,
+        token_a_offered_amount: u64,
+        token_b_wanted_amount: u64,
+    ) -> Result<()> {
+        // vault is created and send offered tokens to vault
+        instructions::make_offer::send_offered_tokens_to_vault(&ctx, token_a_offered_amount)?;
+        // offer is savec with its metadata
+        instructions::make_offer::save_offer(ctx, id, token_b_wanted_amount)
+    }
+
+    pub fn take_offer(ctx: Context<TakeOffer>) -> Result<()> {
+        instructions::take_offer::send_wanted_tokens_to_maker(&ctx)?;
+
+        instructions::take_offer::withdraw_and_close_vault(&ctx)?;
+
+        Ok(())
+    }
+}
