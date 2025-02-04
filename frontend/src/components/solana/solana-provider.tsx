30fc27f239969a0f5506323204f8a9eb330fa3e6
Jaikumar mohite <mohitejaikumar@gmail.com>
2025-02-04 21:13:36 +0000

    Refactor imports

--- frontend/src/components/solana/solana-provider.tsx
+++ frontend/src/components/solana/solana-provider.tsx
@@ -14,7 +14,7 @@ import { WalletModalProvider } from '@solana/wallet-adapter-react-ui';
 import { ReactNode, useCallback, useMemo } from 'react';
 import { clusterApiUrl } from '@solana/web3.js';
 
-require('@solana/wallet-adapter-react-ui/styles.css');
+import '@solana/wallet-adapter-react-ui/styles.css';
 
 export const WalletButton = dynamic(
   async () =>
